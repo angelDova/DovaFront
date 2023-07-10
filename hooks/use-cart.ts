0a7f448e6ee411c29d12d384bd5864ee845c7fdc
angelDova <Angelfcb01@yahoo.com>
2023-07-09 23:43:33 -0700

    update

--- hooks/use-cart.ts
+++ hooks/use-cart.ts
@@ -1,8 +1,9 @@
 import { create } from "zustand";
+import { toast } from "react-hot-toast";
 import { persist, createJSONStorage } from "zustand/middleware";
 
 import { Product } from "@/types";
-import { toast } from "react-hot-toast";
+import { AlertTriangle } from "lucide-react";
 
 interface CartStore {
   items: Product[];
@@ -20,15 +21,15 @@ const useCart = create(
         const existingItem = currentItems.find((item) => item.id === data.id);
 
         if (existingItem) {
-          return toast("Item is already in your cart");
+          return toast("Item already in cart.");
         }
 
         set({ items: [...get().items, data] });
-        toast.success("Item added to cart");
+        toast.success("Item added to cart.");
       },
       removeItem: (id: string) => {
         set({ items: [...get().items.filter((item) => item.id !== id)] });
-        toast.success("Item removed from cart");
+        toast.success("Item removed from cart.");
       },
       removeAll: () => set({ items: [] }),
     }),
